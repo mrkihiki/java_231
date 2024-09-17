9bd6ab46257b29e7b9f4e328ec2e6577f3098a0c
mrkihiki <mrkihiki@yandex.ru>
2024-09-18 06:05:38 +0700

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+        List<String> m =m1(500);
+        System.out.println(m);
+
+    }
+    public static List<String> m1(int n){
+        List<String> m1=new ArrayList<>();
+        for (int i=1; i<=n;i++){
+            if (i % 5 == 0){
+                m1.add("fizz");
+                if(i % 7 == 0){
+                    m1.set(i-1,"fizzbuzz");
+                }
+            } else if (i % 7 == 0) {
+                m1.add("buzz");
+            }else {
+                m1.add(String.valueOf(i));
+            }
+
+        }
+        return m1;
+    }
+}
